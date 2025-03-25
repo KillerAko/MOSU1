f65187ed48e922bce84f1498d69c564642d7a2c1
KillerAko <Sashapapin22@gmail.com>
2025-03-25 23:49:49 +0200

    Add project files.

---
+++ MOSU1/Mixer.Designer.cs
@@ -0,0 +1,408 @@
+﻿
+namespace MOSU1
+{
+    partial class Mixer
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Mixer));
+            chartConcentration = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            StartButton = new Button();
+            StopButton = new Button();
+            pictureBox1 = new PictureBox();
+            Timer = new System.Windows.Forms.Timer(components);
+            label1 = new Label();
+            label2 = new Label();
+            G1textBox = new TextBox();
+            G1MinesButton = new Button();
+            G1PlusButton = new Button();
+            C1PlusButton = new Button();
+            C1MinesButton = new Button();
+            C1textBox = new TextBox();
+            label3 = new Label();
+            C2PlusButton = new Button();
+            C2MinesButton = new Button();
+            C2textBox = new TextBox();
+            label4 = new Label();
+            G2PlusButton = new Button();
+            G2MinesButton = new Button();
+            G2textBox = new TextBox();
+            label5 = new Label();
+            SpeedX1 = new Button();
+            SpeedX4 = new Button();
+            SpeedX10 = new Button();
+            SpeedX50 = new Button();
+            SpeedX100 = new Button();
+            ((System.ComponentModel.ISupportInitialize)chartConcentration).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // chartConcentration
+            // 
+            chartArea1.Name = "ChartArea1";
+            chartConcentration.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            chartConcentration.Legends.Add(legend1);
+            chartConcentration.Location = new Point(249, 476);
+            chartConcentration.Name = "chartConcentration";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "c(t)";
+            chartConcentration.Series.Add(series1);
+            chartConcentration.Size = new Size(846, 450);
+            chartConcentration.TabIndex = 0;
+            chartConcentration.Text = "chart1";
+            // 
+            // StartButton
+            // 
+            StartButton.Location = new Point(43, 655);
+            StartButton.Name = "StartButton";
+            StartButton.Size = new Size(142, 111);
+            StartButton.TabIndex = 1;
+            StartButton.Text = "Start";
+            StartButton.UseVisualStyleBackColor = true;
+            StartButton.Click += StartButton_Click;
+            // 
+            // StopButton
+            // 
+            StopButton.Location = new Point(43, 772);
+            StopButton.Name = "StopButton";
+            StopButton.Size = new Size(142, 115);
+            StopButton.TabIndex = 2;
+            StopButton.Text = "Stop";
+            StopButton.UseVisualStyleBackColor = true;
+            StopButton.Click += StopButton_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(490, 72);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(396, 359);
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(63, 41);
+            label1.Name = "label1";
+            label1.Size = new Size(74, 38);
+            label1.TabIndex = 4;
+            label1.Text = "Gin1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(1169, 401);
+            label2.Name = "label2";
+            label2.Size = new Size(94, 38);
+            label2.TabIndex = 5;
+            label2.Text = "Speed";
+            // 
+            // G1textBox
+            // 
+            G1textBox.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            G1textBox.Location = new Point(43, 82);
+            G1textBox.Name = "G1textBox";
+            G1textBox.Size = new Size(119, 45);
+            G1textBox.TabIndex = 7;
+            G1textBox.Text = "0";
+            // 
+            // G1MinesButton
+            // 
+            G1MinesButton.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            G1MinesButton.Location = new Point(43, 133);
+            G1MinesButton.Name = "G1MinesButton";
+            G1MinesButton.Size = new Size(60, 52);
+            G1MinesButton.TabIndex = 8;
+            G1MinesButton.Text = "-";
+            G1MinesButton.UseVisualStyleBackColor = true;
+            G1MinesButton.Click += G1MinesButton_Click;
+            // 
+            // G1PlusButton
+            // 
+            G1PlusButton.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            G1PlusButton.Location = new Point(102, 133);
+            G1PlusButton.Name = "G1PlusButton";
+            G1PlusButton.Size = new Size(60, 52);
+            G1PlusButton.TabIndex = 9;
+            G1PlusButton.Text = "+";
+            G1PlusButton.UseVisualStyleBackColor = true;
+            G1PlusButton.Click += G1PlusButton_Click;
+            // 
+            // C1PlusButton
+            // 
+            C1PlusButton.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            C1PlusButton.Location = new Point(308, 133);
+            C1PlusButton.Name = "C1PlusButton";
+            C1PlusButton.Size = new Size(60, 52);
+            C1PlusButton.TabIndex = 13;
+            C1PlusButton.Text = "+";
+            C1PlusButton.UseVisualStyleBackColor = true;
+            C1PlusButton.Click += C1PlusButton_Click;
+            // 
+            // C1MinesButton
+            // 
+            C1MinesButton.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            C1MinesButton.Location = new Point(249, 133);
+            C1MinesButton.Name = "C1MinesButton";
+            C1MinesButton.Size = new Size(60, 52);
+            C1MinesButton.TabIndex = 12;
+            C1MinesButton.Text = "-";
+            C1MinesButton.UseVisualStyleBackColor = true;
+            C1MinesButton.Click += C1MinesButton_Click;
+            // 
+            // C1textBox
+            // 
+            C1textBox.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            C1textBox.Location = new Point(249, 82);
+            C1textBox.Name = "C1textBox";
+            C1textBox.Size = new Size(119, 45);
+            C1textBox.TabIndex = 11;
+            C1textBox.Text = "0";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(271, 41);
+            label3.Name = "label3";
+            label3.Size = new Size(72, 38);
+            label3.TabIndex = 10;
+            label3.Text = "Cin1";
+            // 
+            // C2PlusButton
+            // 
+            C2PlusButton.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            C2PlusButton.Location = new Point(308, 312);
+            C2PlusButton.Name = "C2PlusButton";
+            C2PlusButton.Size = new Size(60, 52);
+            C2PlusButton.TabIndex = 21;
+            C2PlusButton.Text = "+";
+            C2PlusButton.UseVisualStyleBackColor = true;
+            C2PlusButton.Click += C2PlusButton_Click;
+            // 
+            // C2MinesButton
+            // 
+            C2MinesButton.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            C2MinesButton.Location = new Point(249, 312);
+            C2MinesButton.Name = "C2MinesButton";
+            C2MinesButton.Size = new Size(60, 52);
+            C2MinesButton.TabIndex = 20;
+            C2MinesButton.Text = "-";
+            C2MinesButton.UseVisualStyleBackColor = true;
+            C2MinesButton.Click += C2MinesButton_Click;
+            // 
+            // C2textBox
+            // 
+            C2textBox.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            C2textBox.Location = new Point(249, 261);
+            C2textBox.Name = "C2textBox";
+            C2textBox.Size = new Size(119, 45);
+            C2textBox.TabIndex = 19;
+            C2textBox.Text = "0";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(271, 220);
+            label4.Name = "label4";
+            label4.Size = new Size(72, 38);
+            label4.TabIndex = 18;
+            label4.Text = "Cin2";
+            // 
+            // G2PlusButton
+            // 
+            G2PlusButton.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            G2PlusButton.Location = new Point(102, 312);
+            G2PlusButton.Name = "G2PlusButton";
+            G2PlusButton.Size = new Size(60, 52);
+            G2PlusButton.TabIndex = 17;
+            G2PlusButton.Text = "+";
+            G2PlusButton.UseVisualStyleBackColor = true;
+            G2PlusButton.Click += G2PlusButton_Click;
+            // 
+            // G2MinesButton
+            // 
+            G2MinesButton.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            G2MinesButton.Location = new Point(43, 312);
+            G2MinesButton.Name = "G2MinesButton";
+            G2MinesButton.Size = new Size(60, 52);
+            G2MinesButton.TabIndex = 16;
+            G2MinesButton.Text = "-";
+            G2MinesButton.UseVisualStyleBackColor = true;
+            G2MinesButton.Click += G2MinesButton_Click;
+            // 
+            // G2textBox
+            // 
+            G2textBox.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            G2textBox.Location = new Point(43, 261);
+            G2textBox.Name = "G2textBox";
+            G2textBox.Size = new Size(119, 45);
+            G2textBox.TabIndex = 15;
+            G2textBox.Text = "0";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.Location = new Point(63, 220);
+            label5.Name = "label5";
+            label5.Size = new Size(74, 38);
+            label5.TabIndex = 14;
+            label5.Text = "Gin2";
+            // 
+            // SpeedX1
+            // 
+            SpeedX1.Location = new Point(1141, 451);
+            SpeedX1.Name = "SpeedX1";
+            SpeedX1.Size = new Size(139, 69);
+            SpeedX1.TabIndex = 22;
+            SpeedX1.Text = "X1";
+            SpeedX1.UseVisualStyleBackColor = true;
+            SpeedX1.Click += SpeedX1_Click_1;
+            // 
+            // SpeedX4
+            // 
+            SpeedX4.Location = new Point(1141, 526);
+            SpeedX4.Name = "SpeedX4";
+            SpeedX4.Size = new Size(139, 69);
+            SpeedX4.TabIndex = 23;
+            SpeedX4.Text = "X4";
+            SpeedX4.UseVisualStyleBackColor = true;
+            SpeedX4.Click += SpeedX4_Click_1;
+            // 
+            // SpeedX10
+            // 
+            SpeedX10.Location = new Point(1141, 601);
+            SpeedX10.Name = "SpeedX10";
+            SpeedX10.Size = new Size(139, 69);
+            SpeedX10.TabIndex = 24;
+            SpeedX10.Text = "X10";
+            SpeedX10.UseVisualStyleBackColor = true;
+            SpeedX10.Click += SpeedX10_Click_1;
+            // 
+            // SpeedX50
+            // 
+            SpeedX50.Location = new Point(1141, 676);
+            SpeedX50.Name = "SpeedX50";
+            SpeedX50.Size = new Size(139, 69);
+            SpeedX50.TabIndex = 25;
+            SpeedX50.Text = "X50";
+            SpeedX50.UseVisualStyleBackColor = true;
+            SpeedX50.Click += SpeedX50_Click;
+            // 
+            // SpeedX100
+            // 
+            SpeedX100.Location = new Point(1141, 751);
+            SpeedX100.Name = "SpeedX100";
+            SpeedX100.Size = new Size(139, 69);
+            SpeedX100.TabIndex = 26;
+            SpeedX100.Text = "X100";
+            SpeedX100.UseVisualStyleBackColor = true;
+            SpeedX100.Click += SpeedX100_Click;
+            // 
+            // Mixer
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1332, 938);
+            Controls.Add(SpeedX100);
+            Controls.Add(SpeedX50);
+            Controls.Add(SpeedX10);
+            Controls.Add(SpeedX4);
+            Controls.Add(SpeedX1);
+            Controls.Add(C2PlusButton);
+            Controls.Add(C2MinesButton);
+            Controls.Add(C2textBox);
+            Controls.Add(label4);
+            Controls.Add(G2PlusButton);
+            Controls.Add(G2MinesButton);
+            Controls.Add(G2textBox);
+            Controls.Add(label5);
+            Controls.Add(C1PlusButton);
+            Controls.Add(C1MinesButton);
+            Controls.Add(C1textBox);
+            Controls.Add(label3);
+            Controls.Add(G1PlusButton);
+            Controls.Add(G1MinesButton);
+            Controls.Add(G1textBox);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Controls.Add(StopButton);
+            Controls.Add(StartButton);
+            Controls.Add(chartConcentration);
+            Name = "Mixer";
+            RightToLeft = RightToLeft.Yes;
+            Text = "Mixer";
+            ((System.ComponentModel.ISupportInitialize)chartConcentration).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartConcentration;
+        private Button StartButton;
+        private Button StopButton;
+        private PictureBox pictureBox1;
+        private System.Windows.Forms.Timer Timer;
+        private Label label1;
+        private Label label2;
+        private TextBox G1textBox;
+        private Button G1MinesButton;
+        private Button G1PlusButton;
+        private Button C1PlusButton;
+        private Button C1MinesButton;
+        private TextBox C1textBox;
+        private Label label3;
+        private Button C2PlusButton;
+        private Button C2MinesButton;
+        private TextBox C2textBox;
+        private Label label4;
+        private Button G2PlusButton;
+        private Button G2MinesButton;
+        private TextBox G2textBox;
+        private Label label5;
+        private Button SpeedX1;
+        private Button SpeedX4;
+        private Button SpeedX10;
+        private Button SpeedX50;
+        private Button SpeedX100;
+    }
+}
---
+++ MOSU1/Mixer.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace MOSU1
+{
+    public partial class Mixer : Form
+    {
+        private double V = 3.0; // Об'єм змішувача (м^3)
+        private double rho = 1000.0; // Густина рідини (кг/м^3)
+        private double G_in1, G_in2, C_in1, C_in2;
+        private double concentration;
+        private double time = 0;
+        private double dt = 1; // Крок часу (сек)
+        private System.Windows.Forms.Timer simulationTimer;
+
+        public Mixer()
+        {
+            InitializeComponent();
+
+            // Ініціалізація таймера
+            simulationTimer = new System.Windows.Forms.Timer();
+            simulationTimer.Interval = 1000; // Оновлення кожні 100 мс
+            simulationTimer.Tick += SimulationStep; // Прив'язка обробника подій
+
+            InitializeSimulation();
+        }
+
+        private void InitializeSimulation()
+        {
+            G_in1 = 2.0;
+            G_in2 = 2.0;
+            C_in1 = 0.5;
+            C_in2 = 0.6;
+            concentration = (G_in1 * C_in1 + G_in2 * C_in2) / (G_in1 + G_in2);
+
+            chartConcentration.ChartAreas[0].AxisX.Title = "Час (с)";
+            chartConcentration.ChartAreas[0].AxisY.Title = "Концентрація (кг/кг)";
+            chartConcentration.Series[0].ChartType = SeriesChartType.Line;
+            G1textBox.Text = G_in1.ToString("F2");
+            G2textBox.Text = G_in2.ToString("F2");
+            C1textBox.Text = C_in1.ToString("F2");
+            C2textBox.Text = C_in2.ToString("F2");
+        }
+
+        private void SimulationStep(object sender, EventArgs e)
+        {
+            // Обчислення зміни концентрації за диференційним рівнянням
+            double dC_dt = (G_in1 * C_in1 + G_in2 * C_in2 - (G_in1 + G_in2) * concentration) / (rho * V);
+            concentration += dC_dt * dt;
+            time += dt;
+
+            // Додавання точки на графік
+            chartConcentration.Series[0].Points.AddXY(time, concentration);
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            simulationTimer.Start();
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            simulationTimer.Stop();
+        }
+
+        private void G1PlusButton_Click(object sender, EventArgs e)
+        {
+            G_in1 += 0.1;
+            G1textBox.Text = G_in1.ToString("F2");
+        }
+
+        private void G1MinesButton_Click(object sender, EventArgs e)
+        {
+            G_in1 -= 0.1;
+            G1textBox.Text = G_in1.ToString("F2");
+        }
+
+        private void C1PlusButton_Click(object sender, EventArgs e)
+        {
+            C_in1 += 0.1;
+            C1textBox.Text = C_in1.ToString("F2");
+        }
+
+        private void C1MinesButton_Click(object sender, EventArgs e)
+        {
+            C_in1 -= 0.1;
+            C1textBox.Text = C_in1.ToString("F2");
+        }
+
+        private void G2PlusButton_Click(object sender, EventArgs e)
+        {
+            G_in2 += 0.1;
+            G2textBox.Text = G_in2.ToString("F2");
+        }
+
+        private void G2MinesButton_Click(object sender, EventArgs e)
+        {
+            G_in2 -= 0.1;
+            G2textBox.Text = G_in2.ToString("F2");
+        }
+
+        private void C2PlusButton_Click(object sender, EventArgs e)
+        {
+            C_in2 += 0.1;
+            C2textBox.Text = C_in2.ToString("F2");
+        }
+
+        private void C2MinesButton_Click(object sender, EventArgs e)
+        {
+            C_in2 -= 0.1;
+            C2textBox.Text = C_in2.ToString("F2");
+        }
+
+
+        private void SpeedX1_Click_1(object sender, EventArgs e)
+        {
+            simulationTimer.Interval = 1000; // 1 секунда
+        }
+
+        private void SpeedX4_Click_1(object sender, EventArgs e)
+        {
+            simulationTimer.Interval = 250; // 4 рази швидше (0.25 сек)
+        }
+
+        private void SpeedX10_Click_1(object sender, EventArgs e)
+        {
+            simulationTimer.Interval = 100; // 10 разів швидше (0.1 сек)
+        }
+
+        private void SpeedX50_Click(object sender, EventArgs e)
+        {
+            simulationTimer.Interval = 20; // 50 разів швидше (0.02 сек)
+        }
+
+        private void SpeedX100_Click(object sender, EventArgs e)
+        {
+            simulationTimer.Interval = 1; // 100 разів швидше (0.01 сек)
+        }
+
+        
+    }
+}
